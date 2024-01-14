4e89ca18561668f62a83f1208cdc59ac34bbc6b1
Chad Chapman <chad.chapman2010@gmail.com>
2024-01-14 18:32:38 -0500

    Restored project

--- routes/api.js
+++ routes/api.js
@@ -13,7 +13,7 @@ router.get('/', function(req, res, next) {
   let { query, pathname: path } = url.parse(req.url, true);
   
   // Read api key from file.
-  fs.readFile('apikey.txt', 'utf8', (err, data) => {
+  fs.readFile('apikey.txt', 'utf8', async (err, data) => {
     if (err) {
       console.log(err);
       return res.status(400).send({ message: err });
@@ -26,20 +26,38 @@ router.get('/', function(req, res, next) {
     ];
 
     let weatherApiRequestURL = "";
+    let weatherData;
     if(query.type) {
       if(query.type == "SIMPLE") {
         weatherApiRequestURL = `https://api.openweathermap.org/data/2.5/weather?q=${query.city}&appid=${data}`;
-        console.log("Request URL: " + weatherApiRequestURL);
+        weatherData = await getSimpleData(weatherApiRequestURL);
+
+        console.log(weatherData);
       }
 
       // Send response back to requestor.
       res.json({
-        data: weatherApiRequestURL
+        data: weatherData
       });
     }
 
   });
-  
+  // Set key to object we can send as JSON response.
+  // let apiKey = [
+  //   { key: {data} }
+  // ];
+  // // Send response back to requestor.
+  // res.json({
+  //   data: apiKey 
+  // });
 });
 
+async function getSimpleData(requestURL) {
+  try {
+    const response = await fetch(requestURL);
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
 module.exports = router;
