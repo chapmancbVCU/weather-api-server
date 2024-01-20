05ce8673f49b7de52d47b5fbe272442f7153e16f
Chad Chapman <chad.chapman2010@gmail.com>
2024-01-19 21:27:05 -0500

    Added support for onecall requests at Open Weather Map

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 /**
+ * @file Route for handling Open Weather Map API requests.
  * @author Chad Chapman
  */
 const express = require('express');
@@ -30,10 +31,13 @@ router.get('/', function(req, res, next) {
     if(query.type) {
       if(query.type == "SIMPLE") {
         weatherApiRequestURL = `https://api.openweathermap.org/data/2.5/weather?q=${query.city}&appid=${data}`;
-        weatherData = await getSimpleData(weatherApiRequestURL);
-
         console.log(weatherData);
+      } else if(query.type == "COMPLETE") {
+        weatherApiRequestURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${query.lat}&lon=${query.lon}&units=${query.units}&appid=${data}`;
       }
+
+      weatherData = await getWeaterData(weatherApiRequestURL);
+      console.log(weatherData);
 
       // Send response back to requestor.
       res.json({
@@ -42,17 +46,16 @@ router.get('/', function(req, res, next) {
     }
 
   });
-  // Set key to object we can send as JSON response.
-  // let apiKey = [
-  //   { key: {data} }
-  // ];
-  // // Send response back to requestor.
-  // res.json({
-  //   data: apiKey 
-  // });
 });
 
-async function getSimpleData(requestURL) {
+
+/**
+ * 
+ * @param {string} requestURL The url that is used for the fetch request will 
+ * send to Open Weather Map. 
+ * @returns A JSON string containing the response from Open Weather Map.
+ */
+async function getWeaterData(requestURL) {
   try {
     const response = await fetch(requestURL);
     return await response.json();
@@ -60,4 +63,6 @@ async function getSimpleData(requestURL) {
     console.log(error);
   }
 }
+
+
 module.exports = router;
