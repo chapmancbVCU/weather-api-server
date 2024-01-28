6c22ef0d883bb3142c3336d0a4ca792cf1963bcb
Chad Chapman <chad.chapman2010@gmail.com>
2024-01-27 22:49:39 -0500

    Performed following updates: * Began to add support for API calls for getting location information when user performs a search * Optimization of code by removing unnecessary variables and reducing code to fewer lines when possible * Spacing fixes after updating tab settings

--- routes/api.js
+++ routes/api.js
@@ -10,43 +10,42 @@ const url = require('url');
 
 /* GET api listing. */
 router.get('/', async function(req, res, next) {
-  // Parse query section of request.
-  let { query, pathname: path } = url.parse(req.url, true);
-  
-  console.log("\n\n--------------------------------------------------------------------------------");
-  console.log("QUERY INFORMATION");
-  console.log(query);
-
-  let data;
-  // Set response data to object we can send as JSON response.
-  let responseData = [
-    { key: {data} }
-  ];
-
-  let weatherApiRequestURL = "";
-  let weatherData;
-
-  // Determine what type of request and handle appropriately.
-  if(query.type) {
-    if(query.type == "SIMPLE") {
-      weatherApiRequestURL = `https://api.openweathermap.org/data/2.5/weather?q=${query.city}&appid=${process.env.OPEN_WEATHER_MAP_API_KEY}`;
-      weatherData = await getWeatherData(weatherApiRequestURL);
-    } else if(query.type == "ONECALL") {
-      weatherApiRequestURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${query.lat}&lon=${query.lon}&units=${query.units}&appid=${process.env.OPEN_WEATHER_MAP_API_KEY}`;
-      weatherData = await getWeatherData(weatherApiRequestURL);
-    } else {
-      let badRequestObj = { message: "Invalid request type.  We currently support the free SIMPLE and ONECALL fetch requests."}
-      weatherData = JSON.stringify(badRequestObj);
-    }
-
-    console.log("\nQUERY RESPONSE");
-    console.log(weatherData);
+    // Parse query section of request.
+    let { query, pathname: path } = url.parse(req.url, true);
     
-    // Send response back to requestor.
-    res.json({
-      data: weatherData
-    });
-  }
+    console.log("\n\n--------------------------------------------------------------------------------");
+    console.log("QUERY INFORMATION");
+    console.log(query);
+
+    let data;
+    // Set response data to object we can send as JSON response.
+    let responseData = [
+        { key: {data} }
+    ];
+
+    // Declare variable for data here for use later.
+    let weatherAPIData;
+
+    // Determine what type of request and handle appropriately.
+    if(query.type) {
+        if(query.type === "SIMPLE") {
+            weatherAPIData = await getWeatherAPIData(`https://api.openweathermap.org/data/2.5/weather?q=${query.city}&appid=${process.env.OPEN_WEATHER_MAP_API_KEY}`);
+        } else if(query.type === "ONECALL") {
+            weatherAPIData = await getWeatherAPIData(`https://api.openweathermap.org/data/3.0/onecall?lat=${query.lat}&lon=${query.lon}&units=${query.units}&appid=${process.env.OPEN_WEATHER_MAP_API_KEY}`);
+        } else if(query.type === "SEARCH_TERM") {
+            weatherApiRequestURL = `http://api.openweathermap.org/geo/1.0/direct?q=${query.searchTerm}&limit=${process.env.SEARCH_TERM_LIMIT}&appid=${process.env.OPEN_WEATHER_MAP_API_KEY}`;
+        } else {
+            weatherAPIData = JSON.stringify({ message: "Invalid request type.  We currently support the free SIMPLE, ONECALL, and geolocation API requests from Open Weather Map."});
+        }
+
+        console.log("\nQUERY RESPONSE");
+        console.log(weatherAPIData);
+        
+        // Send response back to requestor.
+        res.json({
+        data: weatherAPIData
+        });
+    }
 });
 
 
@@ -57,13 +56,13 @@ router.get('/', async function(req, res, next) {
  * send to Open Weather Map. 
  * @returns A JSON string containing the response from Open Weather Map.
  */
-async function getWeatherData(requestURL) {
-  try {
-    const response = await fetch(requestURL);
-    return await response.json();
-  } catch (error) {
-    console.log(error);
-  }
+async function getWeatherAPIData(requestURL) {
+    try {
+        const response = await fetch(requestURL);
+        return await response.json();
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 
